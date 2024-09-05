c9f34f7a615af7ff857fce211e9f14db7d3c9461
omosoclinton <clintonomoso@gmail.com>
2024-09-05 11:51:23 +0300

    Added password reset mechanism

--- django/blog/blog/settings.py
+++ django/blog/blog/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 #TEMPLATE_DIR = Path(BASE_DIR, 'templates')
@@ -135,3 +137,10 @@ LOGIN_REDIRECT_URL = "blog-home" #added
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASS'))
